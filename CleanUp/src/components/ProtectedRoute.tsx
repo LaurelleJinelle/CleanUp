11b077ad8244df1678012e22bd575407c9881145
LaurelleJinelle <j.nformi1@alustudent.com>
2025-03-17 16:11:10 +0200

    It still doen't work

---
+++ CleanUp/src/components/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+const ProtectedRoute = ({ children, user, role }) => {
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  if (user.role !== role) {
+    return <Navigate to={`/${user.role}`} replace />;
+  }
+  return children;
+};
+export default ProtectedRoute;
